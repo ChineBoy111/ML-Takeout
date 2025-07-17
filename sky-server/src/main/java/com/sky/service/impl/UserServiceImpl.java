04f7e161b65cd8dfad58b4e79230cb9e445b64bf
ChineBoy111 <1985914344@qq.com>
2025-07-17 14:21:08 +0800

    小程序登录功能实现

---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    private static final String WX_LOGIN_URL = "https://api.weixin.qq.com/sns/jscode2session";
+
+    private String login(String code){
+        Map<String, String> map = new HashMap<>();
+        map.put("appid", weChatProperties.getAppid());
+        map.put("secret", weChatProperties.getSecret());
+        map.put("js_code", code);
+        map.put("grant_type", "authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN_URL, map);
+
+        JSONObject jsonObject = JSON.parseObject(json);
+        return jsonObject.getString("openid");
+    }
+    @Override
+    public User wxlogin(UserLoginDTO userLoginDTO) {
+        //获得微信小程序用户唯一标识 openid
+        String openid = login(userLoginDTO.getCode());
+        if(openid == null){
+            throw  new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+        User user = userMapper.getByOpenid(openid);
+        //新用户自动完成注册
+        if(user == null){
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+        return user;
+    }
+}
