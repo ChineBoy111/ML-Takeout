632f4b4a96f45895a9192b74bb3d62494087f06b
ChineBoy111 <1985914344@qq.com>
2025-07-19 11:42:05 +0800

    支付功能实现

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,10 +1,14 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.Result;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
 
 public interface OrderService {
     Result<OrderSubmitVO> submit(OrdersSubmitDTO ordersSubmitDTO);
+
+    OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO);
 }
